7d652cd9348eb4063188b9e80b69d401a73290db
Dwaraka-420 <bissati.venkata@pditechnologies.com>
2025-02-05 00:25:05 +0530

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+      customNav: '#07003D', // Add your custom color here
+      },
+    },
+  },
+  plugins: [],
+};
